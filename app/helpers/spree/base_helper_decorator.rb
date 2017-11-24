a22a43bf820c690d2f9628868ef17b4ad87be82d
lewington-pitsos <lewington@student.unimelb.edu.au>
2017-11-24 09:12:31 +0700

    buttons styled

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,4 +2,11 @@ module Spree::BaseHelper
     def logo(image_path = Spree::Config[:logo], img_options: {})
       link_to image_tag(image_path, img_options), spree.root_path
     end
+
+    def nav_tree(root_taxon, current_taxon, max_level = 1)
+      return '' if max_level < 1 || root_taxon.children.empty?
+      content_tag :li, class: 'category' do
+       link_to(root_taxon.name, seo_url(root_taxon))
+      end
+    end
 end
