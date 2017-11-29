c57b2b1dabee82ae20f83ae2117819a94f540e88
lewington-pitsos <lewington@student.unimelb.edu.au>
2017-11-29 16:40:57 +0700

    extras leave breadcrumbs

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -8,4 +8,28 @@ module Spree::BaseHelper
        link_to(root_taxon.name, seo_url(root_taxon))
       end
     end
+
+    def taxon_breadcrumbs(taxon, separator = '&nbsp;&raquo;&nbsp;', breadcrumb_class = 'inline')
+      return '' if current_page?('/') || taxon.nil?
+
+      crumbs = [[t('spree.home'), spree.root_path]]
+
+      if taxon
+        crumbs += taxon.ancestors.collect { |a| [a.name, spree.nested_taxons_path(a.permalink)] } unless taxon.ancestors.empty?
+        crumbs << [taxon.name, spree.nested_taxons_path(taxon.permalink)]
+      end
+
+      separator = raw(separator)
+
+      items = crumbs.each_with_index.collect do |crumb, i|
+        content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem') do
+          link_to(crumb.last, itemprop: 'item') do
+            content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (i + 1).to_s }, false, false)
+          end + (crumb == crumbs.last ? '' : separator)
+        end
+      end
+
+      content_tag(:nav, content_tag(:ol, raw(items.map(&:mb_chars).join), class: breadcrumb_class, itemscope: '', itemtype: 'https://schema.org/BreadcrumbList'), id: 'breadcrumbs', class: 'sixteen columns')
+    end
+
 end
