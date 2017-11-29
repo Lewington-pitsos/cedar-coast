d9e172d34333c26477a0d86c8de05d6c8601a64a
lewington-pitsos <lewington@student.unimelb.edu.au>
2017-11-29 11:22:49 +0700

    extras show on homepage

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,7 +1,18 @@
 Spree::HomeController.class_eval do
+  helper 'spree/products'
+  helper 'spree/taxons'
+  respond_to :html
+
   def about
     #@searcher = build_searcher(params.merge(include_images: true))
     #@products = @searcher.retrieve_products
     #@taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+
+  def index
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+    @current_taxon = @taxonomies.last.root
   end
 end
