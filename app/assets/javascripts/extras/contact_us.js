d407fc612b5d70c3f63ebe2052188be1ea19385b
lewington-pitsos <lewington@student.unimelb.edu.au>
2017-11-30 08:13:55 +0700

    page top button added

--- app/assets/javascripts/extras/contact_us.js
+++ app/assets/javascripts/extras/contact_us.js
@@ -3,4 +3,8 @@ $(window).load(function() {
     e.preventDefault();
     $("html, body").animate({ scrollTop: $(document).height() }, 300);
   })
+  $('#page-top').on('click', function(e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, 300);
+  })
 });
