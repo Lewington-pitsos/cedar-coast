8c623ea297bee4578f8109353eb1938199cb581e
lewington-pitsos <lewington@student.unimelb.edu.au>
2017-12-02 09:16:00 +0700

    fixed header juddering

--- app/assets/javascripts/contact_us.js
+++ app/assets/javascripts/contact_us.js
@@ -1,6 +1,7 @@
-$(window).load(function() {
+$(document).ready(function() {
   $(document).on('click', function(e) {
-    if ($(e.target).attr('id') == 'page-top') {      e.preventDefault();
+    if ($(e.target).attr('id') == 'page-top') {
+      e.preventDefault();
       $("html, body").animate({ scrollTop: 0 }, 300);
     } else if ($(e.target).attr('class') == 'contact-us') {
       e.preventDefault();
